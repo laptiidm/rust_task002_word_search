918bf486ec58e6754b92b3929dce9919237c3e06
Dmytro <laptii.dm@gmail.com>
2023-04-12 17:08:36 +0300

    return an unordered html document

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
 use reqwest;
+//use scraper::Html;
 use tokio;
 
 #[tokio::main]
 async fn main() {
-    
-    let url = "https://doc.rust-lang.ru/stable/rust-by-example/meta/doc.html";
-    let _response = fetch_url(url).await;
-    //println!("{:?}", response);
-    
+    let url = "https:allo.ua";
+    let html = fetch_url(url).await.unwrap();
+    println!("{:?}", html);
 }
 
 async fn fetch_url(url: &str) -> Result<String, reqwest::Error> {
     let response = reqwest::get(url).await?.text().await?;
-    println!("{}", response);
+
     Ok(response)
 }
-
-
-
-
-
-
-
-
-//https://www.thetimes.co.uk/
